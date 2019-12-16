f0485d7d8e34ecfbe3459af56bf925e33bc06a36
corentin8 <58816557+corentin8@users.noreply.github.com>
2019-12-16 13:46:39 +0100

    elfread update fonctionnel (jusqu'a la partie 3)

--- elf_linker-1.0/ELFRead.c
+++ elf_linker-1.0/ELFRead.c
@@ -27,33 +27,32 @@ void printSh_Type(Elf32_Word leMot){
         case  17:   printf("type de la section: Section group \n");break;
         case  18:   printf("type de la section: Extended section indeces \n");break;
         case  19:   printf("type de la section: Number of defined types \n");break;
-        default :   printf("(non Traité)type de la Section: %d \n",leMot);break;
+        default :   printf("(non Traité)type de la Section: %d",leMot);break;
     }
 }
 
 void printFlagz(Elf32_Word leMot){
     switch(leMot){
-        case (1 << 0)   :   printf("flag: Writable \n");break;
-        case (1 << 1)   :   printf("flag:Occupies memory during execution\n");break;
-        case (1 << 2)   :   printf("flag:Executable\n");break;
-        case (1 << 4)   :   printf("flag:Might be merged\n");break;
-        case (1 << 5)   :   printf("flag:Contains nul-terminated strings\n");break;
-        case (1 << 6)   :   printf("flag:`sh_info' contains SHT index\n");break;
-        case (1 << 7)   :   printf("flag:Preserve order after combining\n");break;
-        case (1 << 8)   :   printf("flag:Non-standard OS specific handling required\n");break;
-        case (1 << 9)   :   printf("flag:Section is member of a group\n");break;
-        case (1 << 10)  :   printf("flag:Section hold thread-local data.\n");break;
-        case (1 << 11)  :   printf("flag:Section with compressed data.\n");break;
-        case 0x0ff00000 :   printf("flag:OS-specific.\n");break;
-        case 0xf0000000 :   printf("flag:Processor-specific\n");break;
-        case (1 << 30)  :   printf("flag:Special ordering requirement(Solaris)\n"); break;
-        case (1U << 31) :   printf("flag:Section is excluded unless referenced or allocated (Solaris)\n");break;
+        case (1 << 0)   :   printf("Writable");break;
+        case (1 << 1)   :   printf("Occupies memory during execution");break;
+        case (1 << 2)   :   printf("Executable");break;
+        case (1 << 4)   :   printf("Might be merged");break;
+        case (1 << 5)   :   printf("Contains nul-terminated strings");break;
+        case (1 << 6)   :   printf("`sh_info' contains SHT index");break;
+        case (1 << 7)   :   printf("Preserve order after combining");break;
+        case (1 << 8)   :   printf("Non-standard OS specific handling required");break;
+        case (1 << 9)   :   printf("Section is member of a group");break;
+        case (1 << 10)  :   printf("Section hold thread-local data.");break;
+        case (1 << 11)  :   printf("Section with compressed data.");break;
+        case 0x0ff00000 :   printf("OS-specific.");break;
+        case 0xf0000000 :   printf("Processor-specific");break;
+        case (1 << 30)  :   printf("Special ordering requirement(Solaris)"); break;
+        case (1U << 31) :   printf("Section is excluded unless referenced or allocated (Solaris)");break;
     }
 }
 
-Elf32_Shdr * afficherTableSectionHeader(FILE * fp, Elf32_Ehdr header){
+void afficherTableSectionHeader(FILE * fp, Elf32_Ehdr header, Elf32_Shdr ** sheader){
     fseek(fp,header.e_shoff,SEEK_SET);
-    Elf32_Shdr* sheader[header.e_shnum];
     for(int i=0;i<header.e_shnum;i++){
         sheader[i]=(Elf32_Shdr*)malloc(sizeof(Elf32_Shdr));
         fread(sheader[i],1, sizeof(Elf32_Shdr), fp);
@@ -72,7 +71,7 @@ Elf32_Shdr * afficherTableSectionHeader(FILE * fp, Elf32_Ehdr header){
         printf("taille de la section si elle a une table: %d \n",sheader[i]->sh_entsize);
 	printf("\n");
     }
-    return *sheader;
+    return ;
 }
 void freeSHDR(Elf32_Shdr * sheader){
     for (int i=0;i<sizeof(sheader)/sizeof(Elf32_Shdr);i++){
@@ -183,17 +182,14 @@ void affichage_contenu_section (int num,Elf32_Ehdr header,Elf32_Shdr** sheader ,
 	}
 	else {
 		if (sheader[num]->sh_size==0){
-			printf("%d \n",sheader[num]->sh_size);
-			printf("section vide\n");}
+			printf("section vide\n");
+			printf("\n");}
 		else {
-		int * contenu;
+		uint8_t contenu[sheader[num]->sh_size];
 		fseek(f,sheader[num]->sh_offset,SEEK_SET);
-		printf("%d \n",sheader[num]->sh_size);
+		fread(contenu, 1,sheader[num]->sh_size, f);
 		for (int i=0; i<sheader[num]->sh_size; i++){
-			//printf("%d \n",sheader[num]->sh_size);
-			//fread(contenu, 1, 1, f);
-			//fseek(f,1,SEEK_CUR);
-			//printf("%02X ",*contenu);
+			printf("%02X ",contenu[i]);
 		}
 		printf("\n");}
 	}
@@ -212,17 +208,17 @@ int main(int argc, char *argv[]){
 		printf(" /// HEADER ELF /// \n");
 		printf("\n");
     		header = elf_read_entete(src);
-		Elf32_Shdr* sheader;
+		Elf32_Shdr *sheader[header->e_shnum];
 		printf("\n");
 		printf(" /// section HEADER TABLE /// \n");
 		printf("\n");
-		sheader=afficherTableSectionHeader(src,*header);
+		afficherTableSectionHeader(src,*header,sheader);
 		printf("\n");
 		printf(" /// section ELF /// \n");
 		printf("\n");
 		for (int i=0; i<10;i++){
 			printf("section %d \n",i);
-			affichage_contenu_section(i,*header,&sheader,src);}
+			affichage_contenu_section(i,*header,sheader,src);}
 		
   	} else {
    		printf("bad file");
